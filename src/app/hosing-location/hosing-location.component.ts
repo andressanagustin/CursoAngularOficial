bf2209be06a16fc3f92d428b4bc3420a5a664567
andressanagustin <58555998+andressanagustin@users.noreply.github.com>
2024-10-15 13:27:28 -0300

    first commit

---
+++ src/app/hosing-location/hosing-location.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hosing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      hosing-location works!
+    </p>
+  `,
+  styles: ``
+})
+export class HosingLocationComponent {
+
+}
