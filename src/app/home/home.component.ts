3e36135f4efb26de3b808654f5fca1507986b64f
Andres <andressanagustin@gmail.com>
2024-10-15 20:04:06 -0300

    continuacion del curso

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HosingLocationComponent } from '../hosing-location/hosing-location.component';
+import { HousingLocation } from '../housinglocation';
+import {HousingService} from '../housing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,HosingLocationComponent],
   template: `
     <section>
       <form>
@@ -11,9 +15,21 @@ import { Component } from '@angular/core';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
+    <section class="results">
+      <app-hosing-location 
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation"
+      ></app-hosing-location>
+    </section>
   `,
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  housingService: HousingService = inject(HousingService);
 
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+  
+  housingLocationList: HousingLocation[] = [];
 }
